affdb21be41264a5d450dcec00024f0846df0907
Mark Seda <markpseda@gmail.com>
2022-04-16 13:36:33 -0400

    Added code to read data from server, demonstrating the issues with asyncronicity of connection over tcp - unsure when the other side is done sending, timeouts another very poor solution.

--- AsioPlayground/main.cpp
+++ AsioPlayground/main.cpp
@@ -21,6 +21,31 @@ int main()
 	if (socket.is_open())
 	{
 		std::cout << "Connected!" << std::endl;
+
+
+		std::string sRequest =
+			"GET /index.html HTTP/1.1\r\n"
+			"Host: example.com\r\n"
+			"Connection: close\r\n\r\n";
+
+		socket.write_some(boost::asio::buffer(sRequest.data(), sRequest.size()), ec);
+
+		socket.wait(socket.wait_read);
+
+		size_t bytes = socket.available();
+		std::cout << "Bytes available: " << bytes << std::endl;
+
+		if (bytes > 0)
+		{
+			std::vector<char> vBuffer(bytes);
+			socket.read_some(boost::asio::buffer(vBuffer.data(), vBuffer.size()), ec);
+
+			for (auto& c : vBuffer)
+			{
+				std::cout << c;
+			}
+		}
+
 	}
 	else
 	{
